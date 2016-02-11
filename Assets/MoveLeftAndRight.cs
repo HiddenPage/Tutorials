986857590ba291d5600a5e6e2b8cc912f2336bf9
Lisa Farina <lisa.farina@gmail.com>
2016-02-11 12:58:01 -0800

    Initial push with files. Have a cube that shoots spheres. This is all tutorial stuff.

---
+++ Assets/MoveLeftAndRight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftAndRight : MonoBehaviour
+{
+    //This sets a speed that can be changed in unity properties
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //This moves an object left or right on input
+        transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        //transform.position += Vector3.up * Input.GetAxis("Vertical") * Time.deltaTime;
+    }
+}
---
+++ Assets/SpawnOnButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnOnButton : MonoBehaviour
+{
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //when the user presses the fire button, Unity creates a bullet object
+	if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+	}
+}
